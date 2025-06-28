90aac8b7f5a06ecfd57e5b01b58c414e3716c8f8
Jummai <jummyhassan5@gmail.com>
2025-06-28 17:57:54 +0300

    add country card component

--- src/app/pages/country-list/country-list.component.ts
+++ src/app/pages/country-list/country-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CountryCardComponent } from '../../components/country-card/country-card.component';
 
 @Component({
   selector: 'app-country-list',
-  imports: [],
+  imports: [CountryCardComponent],
   templateUrl: './country-list.component.html',
-  styleUrl: './country-list.component.scss'
+  styleUrl: './country-list.component.scss',
 })
-export class CountryListComponent {
-
-}
+export class CountryListComponent {}
